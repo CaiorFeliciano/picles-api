fc6eeb323fa5edcf9fe5b85681e313c2cd300203
CaiorFeliciano <caiorfeliciano@gmail.com>
2024-04-10 21:06:07 -0300

    criação do fluxo de update

--- src/pet/pet.controller.ts
+++ src/pet/pet.controller.ts
@@ -5,7 +5,9 @@ import {
   Get,
   Inject,
   Param,
+  Patch,
   Post,
+  Put,
 } from '@nestjs/common';
 import CreatePetControllerInput from './dtos/create.pet.controller.input';
 import PetTokens from './pet.tokens';
@@ -14,6 +16,9 @@ import CreatePetUseCaseInput from './usecases/dtos/create.pet.usecase.input';
 import { IUseCase } from 'src/domain/iusecase.interface';
 import GetPetByIdUseCaseInput from './usecases/dtos/get.pet.by.id.usecase.input';
 import GetPetByIdUseCaseOutput from './usecases/dtos/get.pet.by.id.usecase.output';
+import UpdatePetControllerInput from './dtos/update.pet.controller.input';
+import UpdatePetByIdUseCaseInput from './usecases/dtos/update.pet.by.id.usecase.input';
+import UpdatePetByIdUseCaseOutput from './usecases/dtos/update.pet.by.id.usecase.output';
 
 @Controller('pet')
 export class PetController {
@@ -27,6 +32,12 @@ export class PetController {
   private readonly getPetByIdUseCase: IUseCase<
     GetPetByIdUseCaseInput,
     GetPetByIdUseCaseOutput
+  >;
+
+  @Inject(PetTokens.updatePetByIdUseCase)
+  private readonly updatePetByIdUseCase: IUseCase<
+    UpdatePetByIdUseCaseInput,
+    UpdatePetByIdUseCaseOutput
   >;
 
   @Post()
@@ -45,5 +56,17 @@ export class PetController {
     } catch (error) {
       throw new BadRequestException(JSON.parse(error.message));
     }
+  }
+
+  @Put(':id')
+  async updatePet(
+    @Body() input: UpdatePetControllerInput,
+    @Param('id') id: string,
+  ): Promise<UpdatePetByIdUseCaseOutput> {
+    const useCaseInput = new UpdatePetByIdUseCaseInput({
+      ...input,
+      id,
+    });
+    return await this.updatePetByIdUseCase.run(useCaseInput);
   }
 }
